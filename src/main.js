6b0850044f372b87da08db1686b3ab1406c32fa5
Guilherme Kenzo Watanabe Fujimura <gkwfuji@gmail.com>
2026-05-22 20:27:57 -0300

    feat: add das principais pastas
    Adição das pastas para a criação do jogo

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { MapaScene } from './scenes/Mapa.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    pixelArt: true, // Mantém os pixels nítidos (estilo Stardew)
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }, // Jogo top-down não tem gravidade para baixo
+            debug: true // Ative como true para ver as caixas de colisão no início
+        }
+    },
+    scene: [MapaScene] // Sua cena principal de exploração
+};
+
+const game = new Phaser.Game(config);
